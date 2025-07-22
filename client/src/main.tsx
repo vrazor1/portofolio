207f1d4ba795f303954019bd8cd663b9714b17c4
vrazor1 <louay878@gmail.com>
2025-07-22 18:54:39 +0000

    feat: add basename to router for github pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,22 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Import your page components
+import Home from "./pages/home";
+import NotFound from "./pages/not-found";
+
+function App() {
+  return (
+    // THE ONLY CHANGE YOU NEED IS ADDING 'basename' TO THIS COMPONENT
+    <BrowserRouter basename="/AIExpertPortfolio">
+      <Routes>
+        {/* Route for the home page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Catch-all route for your 404 Not Found page */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
