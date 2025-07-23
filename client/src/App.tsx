388708824f3acf6743f512cffa26d6385af98a63
vrazor1 <louay878@gmail.com>
2025-07-23 03:48:04 +0000

    try2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter"; // Import Router as WouterRouter
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,20 +6,15 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 
-// Your custom Router component
 function Router() {
   return (
-    // We wrap the Switch with wouter's Router component and provide the base path
-    <WouterRouter base="/AIExpertPortfolio">
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </WouterRouter>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
-// Your App component remains the same
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,22 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-// Import your page components
-import Home from "./pages/home";
-import NotFound from "./pages/not-found";
-
-function App() {
-  return (
-    // THE ONLY CHANGE YOU NEED IS ADDING 'basename' TO THIS COMPONENT
-    <BrowserRouter basename="/AIExpertPortfolio">
-      <Routes>
-        {/* Route for the home page */}
-        <Route path="/" element={<Home />} />
-
-        {/* Catch-all route for your 404 Not Found page */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+createRoot(document.getElementById("root")!).render(<App />);
